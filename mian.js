dc7b3aa69725f31e3363a22c8076bf3c3a6755b7
aanaaclaraa <129890122+aanaaclaraa@users.noreply.github.com>
2023-04-05 12:42:11 -0300

    alteracao

--- mian.js
+++ mian.js
@@ -12,7 +12,7 @@ document.getElementById( "btn-dividir" ).addEventListener( "click" , dividir );
 
 function showcolor()
 {
-    document.body.style.backgroundColor = "red"; 
+    document.body.style.backgroundColor = "yellow"; 
 }
 
 function exibenome()
